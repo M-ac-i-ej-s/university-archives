dd97b788c479edf9ac91c61c0028d8dbbe6f0ff0
M-ac-i-ej-s <maciej.slupianekge@gmail.com>
2023-10-25 15:00:09 +0200

    lab3-finished

---
+++ wspol/lab3/main.py
@@ -0,0 +1,23 @@
+import sys
+
+def count_in_file(filename, word):
+    count = 0
+    with open(filename, "r") as file:
+        content = file.read()
+        lines = content.split("\n")
+        for line in lines:
+            words = line.split(" ")
+            for w in words:
+                if w == word:
+                    count += 1
+            if "\\input" in line:
+                l = len(line)
+                new_file = line[7:l - 1]
+                count += count_in_file(new_file, word)
+    return count
+
+if __name__ == '__main__':
+    p = sys.argv[1] 
+    s = sys.argv[2]
+    result = count_in_file(p, s)
+    print(f"słowo '{s}' występuje {result} razy w pliku {p}.")
