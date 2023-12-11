33d072a8854f3917f3c906070d8d8d4ecded3787
Maciej Słupianek <maciej.slupianekge@gmail.com>
2023-12-11 17:31:13 +0100

    lab9

--- wspol/lab9/main.py
+++ wspol/lab9/main.py
@@ -1,39 +1,51 @@
 import threading
-import sys
-import time
+from threading import Barrier
+import math
 
-primes = []
-threats_amount = 4
-n = 10000
-
-
-def is_prime(n):
-    k = int(n ** 0.5) + 1
-    for i in range(2, k):
-        if n % i == 0:
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
             return False
     return True
 
-
-def find_primes(f, t, barrier):
-    p = []
-    for i in range(f, t):
-        if is_prime(i):
-            p.append(i)
+def find_primes(start, end, result_list, barrier):
+    primes = []
+    for num in range(start, end + 1):
+        if is_prime(num):
+            primes.append(num)
+    result_list.extend(primes)
     barrier.wait()
-    with lock:
-        primes.extend(p)
-    return p
 
+def main():
+    start_range = 2
+    end_range = 10000
+    num_threads = 4  # Możesz dostosować ilość wątków według własnych potrzeb
 
-threads = []
-lock = threading.Lock()
-barrier = threading.Barrier(threats_amount)
+    # Inicjalizacja bariery z ilością wątków
+    barrier = Barrier(num_threads)
 
-for i in range(threats_amount):
-    threads.append(
-        threading.Thread(target=find_primes, args=(i * n // threats_amount, (i + 1) * n // threats_amount, barrier)))
-    threads[i].start()
-barrier.wait()
-print(primes)
-sys.exit()
+    # Przygotowanie listy wynikowej do przechowywania liczb pierwszych
+    result_list = []
+
+    # Rozbicie przedziału na podprzedziały dla każdego wątku
+    step = (end_range - start_range + 1) // num_threads
+    ranges = [(i, i + step - 1) for i in range(start_range, end_range + 1, step)]
+
+    # Tworzenie i uruchamianie wątków
+    threads = []
+    for i in range(num_threads):
+        thread = threading.Thread(target=find_primes, args=(ranges[i][0], ranges[i][1], result_list, barrier))
+        threads.append(thread)
+        thread.start()
+
+    # Czekanie na zakończenie wszystkich wątków
+    for thread in threads:
+        thread.join()
+
+    # Wyświetlanie wyników
+    print("Liczby pierwsze w przedziale", start_range, "do", end_range, "to:", result_list)
+
+if __name__ == "__main__":
+    main()
